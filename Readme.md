a00b88ef177c790ad837c8edc08a19e44a827e83
zkindest <zkindest5@gmail.com>
2022-01-29 18:18:17 +0530

    fix bugs

--- Readme.md
+++ Readme.md
@@ -1,1 +1,5 @@
 # Static html,scss/css,js starter
+
+## requirements
+
+- `node >= 12.20.0` for [copy-webpack-plugin](https://stackoverflow.com/questions/70288069/hookwebpackerror-not-supported-when-copy-webpack-plugin-is-used-on-windows)
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,8 @@
 import '../sass/index.scss';
 // import '../css/index.css';
+import img from '../../public/images/pattern.svg';
+const image = document.createElement('img');
+image.src = img;
+document.body.appendChild(image)
+
 console.log('sd sdfsd fdsd fsdsd f sadfd sdff');
